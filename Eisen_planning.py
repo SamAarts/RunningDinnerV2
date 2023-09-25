318ce886d846b02d3cc94fdc547ddbb2b6acef95
Annerosedegroot <433534@student.fontys.nl>
2023-09-25 12:30:37 +0200

    eisen gemaakt

--- Eisen_planning.py
+++ Eisen_planning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-# 
+# Elke deelnemer eet elk gerecht
 def controleer_gangen(ExcelInput):
     # Lees het Excel-bestand
     df = pd.read_excel(ExcelInput)
@@ -20,11 +20,47 @@ def controleer_gangen(ExcelInput):
             if len(personen_zonder_gang) > 0:
                 print(f"Deze personen hebben geen {gang_naam}gerecht:")
                 print(personen_zonder_gang)
+                
+    def controleer_adressen():
+        deelnemers_met_zelfde_adres = []
+
+        for index, row in df.iterrows():
+            adressen = [row['Voor'], row['Hoofd'], row['Na']]
+            if len(set(adressen)) != len(adressen):
+                deelnemers_met_zelfde_adres.append(row['Bewoner'])
+
+        if not deelnemers_met_zelfde_adres:
+            print("Elke deelnemer eet elke gang op een ander adres.")
+        else:
+            print("Deze deelnemers eten dezelfde gangen op hetzelfde adres:")
+            print(deelnemers_met_zelfde_adres)
+            
 
     # Roep de functie aan voor verschillende gangen
     controleer_gang('Voor')
     controleer_gang('Hoofd')
     controleer_gang('Na')
+    
+    # Controleer adressen
+    controleer_adressen()
 
 # Roep de functie aan met het pad naar je Excel-bestand als argument
-controleer_gangen('Running Dinner eerste oplossing 2022.xlsx')
+#controleer_gangen('Running Dinner eerste oplossing 2022.xlsx')
+
+# elk huishouden dat niet is vrijgesteld van koken, maakt 1 van de 3 gangen.
+# sommige deelnemers hoeven niet te koken en ontvangen dan dus ook geen gasten op hun adres
+
+        
+def vrijstelling(ExcelInput):
+    # Lees het Excel-bestand
+    df = pd.read_excel(ExcelInput)
+    # lijst maken van alle bewoners die niet hoeven te koken
+    
+    niet_koken = []
+    for index, row in df.iterrows():
+        if pd.isna(row['kookt']):
+            niet_koken.append(row['Bewoner'])
+    print(niet_koken)
+    
+vrijstelling('Running Dinner eerste oplossing 2022.xlsx')
+#controleer_niet_kok('Running Dinner eerste oplossing 2022.xlsx')
