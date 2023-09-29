d997d23320dac6d464f76b4b582269b23c09fc7f
SamAarts <505660@student.fontys.nl>
2023-09-29 13:37:32 +0200

    eerste opzet

---
+++ Wensen_planning.py
@@ -0,0 +1,13 @@
+
+
+def Voorkeursgang():
+    import pandas as pd
+
+    # Load the Excel files, drop unnecessary columns, and reset the indices
+    df2022 = pd.read_excel('Running Dinner dataset 2022.xlsx', sheet_name='Adressen').drop(['Min groepsgrootte', 'Max groepsgrootte'], axis=1).reset_index(drop=True)
+    df2021 = pd.read_excel('Running Dinner dataset 2021.xlsx', sheet_name='Adressen').drop(['Min groepsgrootte', 'Max groepsgrootte'], axis=1).reset_index(drop=True)
+    df = df2021.merge(df2022, left_on="Huisadres")
+    print(df)
+
+
+Voorkeursgang()
