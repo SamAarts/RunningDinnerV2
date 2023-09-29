bca4d231d3435e6f14059f73b4fe75a505990aa3
SamAarts <505660@student.fontys.nl>
2023-09-29 10:35:17 +0200

    bijna ongeveer werkend

--- SamFileGroteDFconnecten.py
+++ SamFileGroteDFconnecten.py
@@ -1,32 +1,35 @@
 import pandas as pd
-import numpy as np
 import random
 
+# Read the Excel files into DataFrames
 excelsheet = pd.read_excel('Running Dinner dataset 2022.xlsx', sheet_name='Bewoners') 
 excelsheet2 = pd.read_excel('Running Dinner dataset 2022.xlsx', sheet_name='Adressen')
-df = pd.DataFrame(excelsheet)
-df2 = pd.DataFrame(excelsheet2)
-df = df.drop(columns='Kookt niet')
-df2 = df2.drop(columns='Voorkeur gang')
+
+# Create DataFrames from the Excel sheets
+df = excelsheet.drop(columns='Kookt niet')
+df2 = excelsheet2.drop(columns='Voorkeur gang')
+
+# Merge the DataFrames on 'Huisadres'
 df = df.merge(df2, how='left', on='Huisadres')
+
+# Fill NaN values with 0
 df.fillna(0, inplace=True)
 
-# Aantal gangen, nodig voor the for loop.
+# Define constants
 aantal_gangen = 3
-# Namen van de gangen.
 gang = ['Voorgerecht', 'Hoofdgerecht', 'Nagerecht']
 
 # Create a list of all unique house addresses
 all_houses = df['Huisadres'].unique()
 
-# Initialize the gangen dictionary with empty lists for all house addresses
-gangen = {house: [] for house in all_houses}
+# Initialize a dictionary to track each person's assigned houses
+person_assignments = {}
 
 # Create a list of available individuals
 available_individuals = df['Bewoner'].tolist()
-print(len(available_individuals))
-# For loop where gang_num and gang_naam are taken from the gang list (3 loops).
-for gang_num, gang_naam in enumerate(gang):
+
+# For loop to assign houses to persons
+for gang_naam in gang:
     dfMensInfo = df.sample(frac=1, random_state=42)
     max_grootteHuizen = dfMensInfo['Max groepsgrootte']
     min_grootteHuizen = dfMensInfo['Min groepsgrootte']
@@ -44,50 +47,25 @@ for gang_num, gang_naam in enumerate(gang):
         else:
             random_persons = []
 
-        gangen[house].extend(random_persons)
         available_individuals = [ind for ind in available_individuals if ind not in random_persons]
 
-print(len(gangen.items()))
-for huis, personen in gangen.items():
-    # Print the house and assigned individuals
-    print(f"{huis}: {personen}")
+        # Update the person_assignments dictionary for the current gang
+        for person in random_persons:
+            if person not in person_assignments:
+                person_assignments[person] = {
+                    'Own_House': house,
+                    gang_naam: house,
+                }
+            else:
+                person_assignments[person][gang_naam] = house
 
+# Create a structured DataFrame
+structured_data = []
 
+for person, assignments in person_assignments.items():
+    structured_data.append(assignments)
 
+structured_df = pd.DataFrame(structured_data)
 
-# Initialize a list to keep track of all assigned individuals
-all_assigned_individuals = []
-
-# Initialize the gangen dictionary as an empty dictionary for each house
-gangen = {house: {} for house in all_houses}
-
-# For loop where gang_num and gang_naam are taken from the gang list (3 loops).
-for gang_num, gang_naam in enumerate(gang):
-    dfMensInfo = df.sample(frac=1, random_state=42)
-    max_grootteHuizen = dfMensInfo['Max groepsgrootte']
-    min_grootteHuizen = dfMensInfo['Min groepsgrootte']
-
-    # Iterate over all house addresses
-    for i, house in enumerate(all_houses):
-        max_huisgrootte = int(max_grootteHuizen[i])
-        min_huisgrootte = int(min_grootteHuizen[i])
-
-        # Ensure that max_huisgrootte is at least equal to min_huisgrootte
-        max_huisgrootte = max(max_huisgrootte, min_huisgrootte)
-
-        if max_huisgrootte > 0 and len(available_individuals) >= max_huisgrootte:
-            random_persons = random.sample(available_individuals, max_huisgrootte)
-        else:
-            random_persons = []
-
-        # Check if the gang key exists for this house, and if not, create it
-        if gang_naam not in gangen[house]:
-            gangen[house][gang_naam] = []
-
-        gangen[house][gang_naam].extend(random_persons)
-        available_individuals = [ind for ind in available_individuals if ind not in random_persons]
-
-        # Add assigned individuals to the list of all assigned individuals
-        all_assigned_individuals.extend(random_persons)
-
-
+# Save the resulting DataFrame to an Excel file
+structured_df.to_excel('testmetsam.xlsx', index=False)
