d19d97ef36b89e2b096ef00c0bf0a207263a60e1
SamAarts <505660@student.fontys.nl>
2023-10-02 10:03:24 +0200

    ff refreshen

--- Wensen_planning.py
+++ Wensen_planning.py
@@ -6,7 +6,9 @@ def Voorkeursgang():
     # Load the Excel files, drop unnecessary columns, and reset the indices
     df2022 = pd.read_excel('Running Dinner dataset 2022.xlsx', sheet_name='Adressen').drop(['Min groepsgrootte', 'Max groepsgrootte'], axis=1).reset_index(drop=True)
     df2021 = pd.read_excel('Running Dinner dataset 2021.xlsx', sheet_name='Adressen').drop(['Min groepsgrootte', 'Max groepsgrootte'], axis=1).reset_index(drop=True)
-    df = df2021.merge(df2022, left_on="Huisadres")
+    df = df2021.merge(df2022)
+
+
     print(df)
 
 
